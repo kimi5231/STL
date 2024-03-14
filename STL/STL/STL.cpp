655a0a4a0ff2f286a64f3b3ae082790e68e3d768
kimi5231 <djwjod@naver.com>
2024-03-14 15:15:42 +0900

    2주차 2 실습

--- STL/STL/STL.cpp
+++ STL/STL/STL.cpp
@@ -1,25 +1,29 @@
 //-------------------------------------------------------------------
-// 2024 1학기 STL 화56목56		3월 12일 화요일		(2주 1)
+// 2024 1학기 STL 화56목56		3월 14일 목요일		(2주 2)
 // 
-// 많은 수의 데이터를 처리할 수 있어야...
+// FILE I/O text/binary - high/low level
 //-------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
-#include <algorithm>
+#include <array>
+#include <random>
 #include "save.h"
 
-// [문제] "int값들.txt"에는 몇 개인지 모르는 int값이 있다.
-// 제일 큰 값을 찾아 화면에 출력하라.
+// [문제] "개들"에는 class Dog 객체 100개가 기록되어 있다.
+// 파일은 binary 모드이 write 함수로 sizeof(Dog)*100 바이트를 기록하였다.
+// 파일을 읽어 가장 num값이 큰 Dog 정보를 화면에 출력하라.
+// class Dog의 멤버는 다음과 같다.
+ 
+class Dog
+{
+private:
+	char c;
+	int num;
+};
 
 //-------------
 int main(void)
 //-------------
 {
-	std::ifstream in{ "int값들.txt" };
-	if (not in) 
-		exit(0);
-	
-	std::cout << "최대값: " << *std::max_element(std::istream_iterator<int>{in}, { }) << '\n';
-
 	save("STL.cpp");
 }

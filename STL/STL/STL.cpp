f030da4916f6e0b328cea256740b9bd72523f8a2
kimi5231 <djwjod@naver.com>
2024-04-02 15:11:17 +0900

    5주차 1 실습

--- STL/STL/STL.cpp
+++ STL/STL/STL.cpp
@@ -1,46 +1,38 @@
 //-------------------------------------------------------------------
-// 2024 1학기 STL 화56목56		3월 28일 목요일		(4주 2)
+// 2024 1학기 STL 화56목56		4월 2일 화요일		(5주 1)
 // 
-// callable type -> 정렬 예제에서 시작
+// 4월 25일 (8주 1일) - 중간시험
 //-------------------------------------------------------------------
 #include <iostream>
-#include <array>
-#include <random>
-#include <print>
+#include <cstring>
 #include "save.h"
 
-// [문제] int 100개를 저장할 공간을 확보하라.
-// int 100개의 값을 [1, 10000] 랜덤값으로 설정하라.
-// int값 100개를 C의 qsort를 사용하여 오름차순으로 정렬하라.
-// 정렬결과를 한 줄에 10개씩 화면에 출력하라.
-
-std::array<int, 100> a;
-std::uniform_int_distribution uid{1, 10000};
-std::default_random_engine dre;
-
-int 정렬방법(const void*, const void*);
+class String
+{
+private:
+	size_t len{};
+	std::unique_ptr<char[]> p{};
+public:
+	String(const char* str) : len(strlen(str)) {
+		p = std::make_unique<char[]>(len);
+		memcpy(p.get(), str, len);
+	}
+	friend std::ostream& operator<<(std::ostream& os, const String& s) {
+		for (size_t i = 0; i < s.len; i++)
+			os << s.p.get()[i];
+		return os;
+	}
+};
 
 //-------------
 int main(void)
 //-------------
 {
-	for (int& num : a)
-		num = uid(dre);
+	String s{ "STL 공부를 위한 클래스" };
+	String t = s;
 
-	// 여기서 qsort로 정렬한다 - qsort는 C함수이지만 generic 함수이다.
-	// qsort( 어디를, 몇개야, 한개의 크기는, 너 만의 정렬방법을 알려줘 );
-
-	int (*함수)(const void*, const void*)  = 정렬방법;
-
-	qsort(a.data(), a.size(), sizeof(int), 함수);
-
-	for (int num : a)
-		std::print("{:8}", num);
+	std::cout << s << '\n';
+	std::cout << t << '\n';
 
 	save("STL.cpp");
-}
-
-int 정렬방법(const void* a, const void* b)
-{
-	return *(int*)b - *(int*)a;
 }

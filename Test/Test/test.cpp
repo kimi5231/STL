cafd42e2ce7590a6219f1af27a0d4dfc77d88618
kimi5231 <djwjod@naver.com>
2024-04-23 00:52:11 +0900

    3주차 강의 내용 정리 완료

--- Test/Test/test.cpp
+++ Test/Test/test.cpp
@@ -1,37 +1,19 @@
+//-------------------------------------------------------------------
+// 2024 1학기 STL 화56목56		3월 19일 화요일		(3주 1)
+// 
+// FILE I/O text/binary - high/low level
+//-------------------------------------------------------------------
 #include <iostream>
-#include <fstream>
 #include <array>
-#include <random>
+#include <string>
 
-// [문제] int 1000개를 "int값들"에 저장하였다.
-// 파일은 binary 모드로 열었고
-// 값은 메모리 크기 그대로 4'000 바이트를 기록하였다.
-// int값 중 제일 큰 값을 찾아 화면에 출력하라.
-
-std::default_random_engine dre;
-std::uniform_int_distribution<int> uidC{ 'a', 'z' };
-std::uniform_int_distribution uidNum{ -9999, 9999 };
-
-class Dog
-{
-	char c{ static_cast<char>(uidC(dre)) };
-	int num{ uidNum(dre) };
-public:
-	friend std::ostream& operator<<(std::ostream& os, const Dog& dog)
-	{
-		return os << "글자: " << dog.c << ", 숫자: " << dog.num;
-	}
-};
+char c1[0x7f'ff'ff'ff]{ 1 };
 
 //-------------
 int main(void)
 //-------------
 {
-	std::array<Dog, 100> dogs;
+	std::cout << "사용할 수 있는 최대 배열의 크기 - " << 0x7f'ff'ff'ff << '\n';
 
-	for (Dog dog : dogs)
-		std::cout << dog << '\n';
-
-	std::ofstream out{ "개들", std::ios::binary };
-	out.write((char*)dogs.data(), sizeof(Dog) * dogs.size());
+	
 }

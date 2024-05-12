1ee1491bd5869fbec37868afa7a1a37ea6aaa4bc
kimi5231 <djwjod@naver.com>
2024-05-12 22:23:55 +0900

    10주차 강의 내용 정리 완료

--- Test/Test/test.cpp
+++ Test/Test/test.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <algorithm>
+#include "String.h"
+
+extern bool 관찰;
 
 template <typename 반복자>
 void f(반복자 iter)
 {
-	std::cout << typeid(iter).name() << '\n';
+	// 반복자의 종류를 판단하는 표준 문장
+	std::cout << typeid(반복자::iterator_concept).name() << '\n';
 }
 
 //-------------
 int main(void)
 //-------------
 {
-	std::vector<int> v;
-	std::ostream_iterator<char> p{ std::cout };
+	std::string s{ "1357924680" };
 
-	f(v.begin());
-	f(p);
+	std::sort(s.rbegin(), s.rend());
+	for (auto i = s.rbegin(); i != s.rend(); ++i)
+		std::cout << *i << ' ';
+	std::cout << '\n';
 }

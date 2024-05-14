b7a8cce82b726dd4cab74ba00e6b182f0b951daf
kimi5231 <djwjod@naver.com>
2024-05-14 15:29:04 +0900

    11주차 1 실습

--- STL/STL/STL.cpp
+++ STL/STL/STL.cpp
@@ -1,22 +1,26 @@
 //-------------------------------------------------------------------
-// 2024 1학기 STL 화56목56		5월 9일 목요일		(10주 2)
+// 2024 1학기 STL 화56목56		5월 14일 화요일		(11주 1)
 // 
-// Iterator - 반복자는 포인터를 일반화 한 것이다.
+// 알고리즘 함수 - 반복자를 인자로 받는다
 // 
 // 6월 6일 목요일 - 강의하겠습니다.
+// 6월 13일 목요일 (15주 2일) - 기말시험
 //-------------------------------------------------------------------
 #include <iostream>
-#include <algorithm>
-#include <string>
+#include <vector>
 #include "save.h"
 #include "String.h"
 
 extern bool 관찰;
 
-template <class 반복자, class 호출가능타입>
-반복자 my_find(반복자 b, 반복자 e, 호출가능타입 f)
+template <class InIter, class OutIter>
+void my_copy(InIter b, InIter e, OutIter d)
 {
-
+	while (b != e) {
+		*d = *b;
+		++b;
+		++d;
+	}
 }
 
 // [실습] 연산자 오버로딩
@@ -24,17 +28,14 @@ template <class 반복자, class 호출가능타입>
 int main(void)
 //-------------
 {
-	String s{ "1357924580" };
-
-	// [문제] 다음 코드가 문제없이 실행되도록 하자.
-	// 이 코드는 [begin(), end()]에서 주어진 조건에 맞는 원소의 위치를 리턴한다.
-	my_find(s.begin(), s.end(), [](char c) {
-		if (std::stoi(c) % 2 == 0)
-			return true;
-		return false;
-		});
-	std::cout < "처음 찾은 짝수의 위치는 " << '\n';
-	std::cout < "처음 찾은 짝수의 값은 " << '\n';
-
 	save("STL.cpp");
+
+	String s{ "STL aldorithm 2024년 5월 14일" };
+	std::vector<char> v{s.getLen()};
+	
+	my_copy(s.begin(), s.end(), back_inserter(v));
+
+	for (char c : v)
+		std::cout << v[i];
+	std::cout << '\n';
 }
